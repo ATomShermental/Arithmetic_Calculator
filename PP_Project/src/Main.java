f5cb3cfaa54222f95e582a64bae8252dc3820f7b
ATomShermental <nike123a44@gmail.com>
2022-10-03 22:55:27 +0300

    Add output in file to plain text

--- PP_Project/src/Main.java
+++ PP_Project/src/Main.java
@@ -5,18 +5,22 @@ import PlainTextRealisation.LexemeBuffer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(new File("input.txt"));
+        FileWriter writer = new FileWriter("output.txt");
         while(scanner.hasNextLine()){
             String buffer = scanner.nextLine();
             List<Lexeme> lexemes = Lexeme.lexemeAnalyze(buffer);
             LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
-            System.out.println(Lexeme.expr(lexemeBuffer));
+            writer.write(Lexeme.expr(lexemeBuffer) + "\n");
         }
+        writer.close();
 
 
 
