0fb5f11ecec5356e10b86f32108c3ce7a95bfe2e
ATomShermental <nike123a44@gmail.com>
2022-11-05 18:15:23 +0300

    Add maven framework support

--- PP_Project/src/main/java/Main.java
+++ PP_Project/src/main/java/Main.java
@@ -1,3 +1,4 @@
+package main.java;
 
 import PlainTextRealisation.Lexeme;
 import PlainTextRealisation.LexemeBuffer;
@@ -14,16 +15,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         File input = new File("input.txt");
-//        String outputStr = "output.txt";
-//        Scanner scanner = new Scanner(input);
-//        FileWriter writer = new FileWriter(outputStr);
-//        while(scanner.hasNextLine()){
-//            String buffer = scanner.nextLine();
-//            List<Lexeme> lexemes = Lexeme.lexemeAnalyze(buffer);
-//            LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
-//            writer.write(Lexeme.expr(lexemeBuffer) + "\n");
-//        }
-//        writer.close();
+        String outputStr = "output.txt";
+        Scanner scanner = new Scanner(input);
+        FileWriter writer = new FileWriter(outputStr);
+        while(scanner.hasNextLine()){
+            String buffer = scanner.nextLine();
+            List<Lexeme> lexemes = Lexeme.lexemeAnalyze(buffer);
+            LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
+            writer.write(Lexeme.expr(lexemeBuffer) + "\n");
+        }
+        writer.close();
         DES.encrypt(input);
         DES.decrypt(new File("encrypted.txt"));
 
