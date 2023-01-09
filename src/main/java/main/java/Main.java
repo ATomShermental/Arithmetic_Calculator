fee4ffa65d8877bfac389a7c04fbed6de48f2019
ATomShermental <nike123a44@gmail.com>
2023-01-09 13:36:51 +0300

    add Spring support and web page

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -2,40 +2,16 @@ package main.java;
 
 import PlainTextImplementation.*;
 import XMLImplementation.XMLDOMParser;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.util.ArrayList;
-
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws IOException, JAXBException {
-//        File input = new File("Assets/input.txt");
-//        String outputStr = "Assets/output.txt";
-//        Scanner scanner = new Scanner(input);
-//        FileWriter writer = new FileWriter(outputStr);
-//        while(scanner.hasNextLine()){
-//            String name = scanner.next();
-//            String buffer = scanner.nextLine();
-//            List<Lexeme> lexemes = Lexeme.lexemeAnalyze(buffer);
-//            LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
-//            writer.write(name + " ");
-//            writer.write(Lexeme.expr(lexemeBuffer) + "\n");
-//        }
-//        writer.close();
-//        DES.encrypt(input);
-//        DES.decrypt(new File( "Assets/encrypted.txt"));
-//
-            Results results = new Results();
-            results.setResults(new ArrayList<Result>());
-            Result result1 = new Result("exp1",6);
-            Result result2 = new Result("exp2",5);
-            results.getResults().add(result1);
-            results.getResults().add(result2);
-
-
-        XMLDOMParser xmldomParser = new XMLDOMParser();
-        xmldomParser.encode("Assets/hello2.xml",results);
-
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
     }
 }
