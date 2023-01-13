ebc038285bc7af4711e8fc65921b949fe9fc0092
ATomShermental <nike123a44@gmail.com>
2023-01-13 12:30:30 +0300

    add unit tests for json implementation

--- src/test/java/JSONImplementation/SimpleJSONParserTest.java
+++ src/test/java/JSONImplementation/SimpleJSONParserTest.java
@@ -1,26 +1,63 @@
 package JSONImplementation;
 
 import PlainTextImplementation.Expression;
+import PlainTextImplementation.Result;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.parser.ParseException;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
-public class SimpleJSONParserTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleJSONParserTest {
+    SimpleJSONParser simpleJSONParser = new SimpleJSONParser();
+    @Test
+    void parse() throws IOException, ParseException {
+        byte[] byteArray = (
+                "[" +
+                "{" +
+                "\"name\":\"expr1\"," +
+                "\"content\":\"7+7\"" +
+                "}," +
+                "{" +
+                "\"name\":\"expr2\"," +
+                "\"content\":\"8*2-(-7)\"" +
+                "}," +
+                "{" +
+                "\"name\":\"expr3\"," +
+                "\"content\":\"8---1\"" +
+                "}" + "]"
+                 ).getBytes(StandardCharsets.UTF_8);
+        List<Expression> expressions = simpleJSONParser.parse(byteArray);
+        assertEquals(3, expressions.size());
+        assertEquals("expr1", expressions.get(0).getName());
+        assertEquals("7+7", expressions.get(0).getContent());
+        assertEquals("expr2", expressions.get(1).getName());
+        assertEquals("8*2-(-7)", expressions.get(1).getContent());
+        assertEquals("expr3", expressions.get(2).getName());
+        assertEquals("8---1", expressions.get(2).getContent());
+    }
 
     @Test
-    public void parse() throws IOException, ParseException {
-
-//        SimpleJSONParser jsonParser = new SimpleJSONParser();
-//        // List<Expression> expressions = jsonParser.parse("Assets/lexemeJSON.json");
-//        List<Expression> expectations = new ArrayList<>();
-//        expectations.add(new Expression("expr1","7+7"));
-//        expectations.add(new Expression("expr2","8*2-(-7)"));
-//        expectations.add(new Expression("expr3","8---1"));
-//
-//        Assert.assertEquals(expressions,expectations);
+    void encode() throws IOException {
+        List<Result> results = Arrays.asList(
+                new Result("result1", 5),
+                new Result("result2", -3)
+        );
+        byte[] encoded = simpleJSONParser.encode(results);
+        String json = new String(encoded, StandardCharsets.UTF_8);
+        assertEquals("""
+                [ {\r
+                  "name" : "result1",\r
+                  "result" : 5\r
+                }, {\r
+                  "name" : "result2",\r
+                  "result" : -3\r
+                } ]""", json);
     }
 }
